de42da79724881a3b59279db01b83579c4f83b44
josh <7510543@gmail.com>
2020-09-04 17:58:12 +0900

    feat(product): save products in redux store
    - ProductList component
    - save products in redux store

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Container, GridList } from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import { gql, useQuery } from '@apollo/client'
-import ProductItem from '@components/shop/ProductItem'
 import Pagination from '@material-ui/lab/Pagination'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@modules/redux'
+import { shopActions } from '@modules/redux/shop'
+import ProductList from '@components/shop/ProductList'
 import { Product } from '../src/models/shop'
 
 export const PRODUCTS_QUERY = gql`
@@ -18,23 +21,25 @@ export const PRODUCTS_QUERY = gql`
 `
 
 export default () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const { setProducts } = shopActions
+  const products = useSelector<RootState, Product[]>(
+    (state) => state.shop.products
+  )
   const [page, setPage] = useState(1)
   const { data } = useQuery<{ products: Product[] }>(PRODUCTS_QUERY)
 
   useEffect(() => {
     if (data?.products?.length > 0) {
-      const startIndex = (page - 1) * 5
-      const endIndex = startIndex + 5
-      setProducts(
-        [...data.products]
-          .sort((a, b) => {
+      dispatch(
+        setProducts(
+          [...data.products].sort((a, b) => {
             return Math.max(Math.min(b.score - a.score, 1), -1)
           })
-          .slice(startIndex, endIndex)
+        )
       )
     }
-  }, [data, page])
+  }, [data])
 
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
@@ -43,14 +48,12 @@ export default () => {
     setPage(value)
   }
 
+  const startIndex = (page - 1) * 5
+  const endIndex = startIndex + 5
+
   return (
     <Container>
-      <GridList cols={3}>
-        {data &&
-          products.map((product) => {
-            return <ProductItem key={product.id} product={product} />
-          })}
-      </GridList>
+      <ProductList products={products.slice(startIndex, endIndex)} />
       <Pagination
         count={Math.floor((data?.products?.length - 1) / 5) + 1}
         page={page}
---
+++ src/components/shop/ProductList.tsx
@@ -0,0 +1,17 @@
+import { GridList } from '@material-ui/core'
+import React from 'react'
+import ProductItem from '@components/shop/ProductItem'
+import { Product } from '../../models/shop'
+
+interface Props {
+  products: Product[]
+}
+export default ({ products }: Props) => {
+  return (
+    <GridList cols={3}>
+      {products.map((product) => {
+        return <ProductItem key={product.id} product={product} />
+      })}
+    </GridList>
+  )
+}
